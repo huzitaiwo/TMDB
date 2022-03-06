74b0f818bfb0b3db2c1b6aeddafa09eee8385956
Im-Hassan-wd <taiwohusayn5@gmail.com>
2022-03-07 00:35:12 +0100

    updated Movie component

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -3,14 +3,16 @@ import './Movie.css'
 
 export default function Movie({ movies, totalPages, page, setPage, isLoading }) {
   return (
-    <div className="popular-movies">
-      {movies.map(movie => (
-        <div>
-          <h2>{movie.title}</h2>
-          <img src={`https://image.tmdb.org/t/p/w500${movie.backdrop_path}`} alt={movie.original_title} />
-        </div>
-      ))}
+    <>
+      <div className="popular-movies">
+        {movies.map(movie => (
+          <div>
+            <h2>{movie.title}</h2>
+            <img src={`https://image.tmdb.org/t/p/w500${movie.backdrop_path}`} alt={movie.original_title} />
+          </div>
+        ))}
+      </div>
       {totalPages !== page && <button className="btn-load-more" onClick={() => setPage(page + 1)}>{isLoading ? 'Loading...' : 'load more'}</button>}
-    </div>
+    </>
   )
 }
