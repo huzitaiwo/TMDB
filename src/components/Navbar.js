26e511485da069b5d56ae9b758c9fcacd6b9cc82
Im-Hassan-wd <taiwohusayn5@gmail.com>
2022-03-06 12:28:17 +0100

    Navbar component

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,11 +1,12 @@
-// styles
 import { Link } from 'react-router-dom'
+
+// styles
 import './Navbar.css'
 
 export default function navbar() {
   return (
-    <nav className='navbar'>
-      <Link to='/'><h1>TMDB</h1></Link>
+    <nav>
+      <Link to='/'><h1 className='logo'>TMDB</h1></Link>
       <ul>
         <li><Link to='/latest'>Latest</Link></li>
         <li><Link to='/upcoming'>Upcoming</Link></li>
