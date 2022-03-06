ba313bfa9cf902069ec9070a469087e8a6a17d23
Im-Hassan-wd <taiwohusayn5@gmail.com>
2022-03-06 12:37:00 +0100

    styling Navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -6,12 +6,14 @@ import './Navbar.css'
 export default function navbar() {
   return (
     <nav>
-      <Link to='/'><h1 className='logo'>TMDB</h1></Link>
-      <ul>
-        <li><Link to='/latest'>Latest</Link></li>
-        <li><Link to='/upcoming'>Upcoming</Link></li>
-        <li><Link to='/rated'>Rated</Link></li>
+      <div className="container">
+        <Link to='/'><h1 className='logo'>TMDB</h1></Link>
+        <ul>
+          <li><Link to='/latest'>Latest</Link></li>
+          <li><Link to='/upcoming'>Upcoming</Link></li>
+          <li><Link to='/rated'>Rated</Link></li>
       </ul>
+      </div>
     </nav>
   )
 }
