c0eb4c16ff72a89c741c67944e024564409252f0
Im-Hassan-wd <taiwohusayn5@gmail.com>
2022-03-07 13:05:28 +0100

    latest

--- src/pages/Latest.js
+++ src/pages/Latest.js
@@ -1,64 +1,7 @@
-import { useState, useEffect } from "react"
-
-// components & pages
-import Movie from "../components/Movie"
-import Filter from "../components/Filter"
+import React from 'react'
 
 export default function Latest() {
-  const [popular, setPopular] = useState([])
-  const [filtered, setFiltered] = useState([])
-  const [genre, setGenre] = useState(0)
-
-  const [isLoading, setIsLoading] = useState(false)
-  const [error, setError] = useState(null)
-
-  const [page, setPage] = useState(1)
-  const [totalPages, setTotalPages] = useState(1)
-
-// https://api.themoviedb.org/3/movie/now_playing?api_key=<<api_key>>&language=en-US&page=1
-  const url = `https://api.themoviedb.org/3/movie/now_playing?api_key=821df521d9494e5d28d041685eeaee64&page=${page}`
-
-  useEffect(() => {
-    fetchPopular()
-  }, [page])
-  
-  const fetchPopular = async () => {
-    setIsLoading(true)
-
-    try {
-      const data = await fetch(url)
-
-      if(!data.ok) {
-        throw new Error(data.statusText)
-      }
-
-      const movies = await data.json()
-
-      setIsLoading(false)
-      setPage(movies.page)
-      setTotalPages(movies.total_pages)
-      setPopular([...popular, ...movies.results])
-      setFiltered([...filtered, ...movies.results])
-
-    }
-    catch(err) {
-      setIsLoading(false)
-      setError(err.message)
-    }
-  }
-
   return (
-    <div className="container">
-      {error && <h2>{error}</h2>}
-      {isLoading && <h2>Loading...</h2>}
-      {filtered && (
-        <Filter setFiltered={setFiltered} genre={genre} setGenre={setGenre} popular={popular} />
-      )}
-      <div className="popular-movies">
-        {filtered.map(movie => {
-          return <Movie key={movie.id} movie={movie} />
-        })}
-      </div>
-    </div> 
+    <div>Latest</div>
   )
 }
