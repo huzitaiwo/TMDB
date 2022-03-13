4e65b9e126186b51b59c2263a44ce852bbd3395a
Im-Hassan-wd <taiwohusayn5@gmail.com>
2022-03-13 19:09:16 +0100

    completed poster component

--- src/components/Poster.js
+++ src/components/Poster.js
@@ -3,7 +3,7 @@ import poster from '../asset/poster.jpg'
 export default function Poster() {
   return (
     <img 
-      style={{ height: "300px", width: "100%", objectFit: "cover" }} 
+      style={{ height: "300px", width: "100%", objectFit: "cover", marginBottom: "30px", filter: "blur(2px)" }} 
       src={poster} 
       alt="poster"
     />
